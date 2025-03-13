7f300b25e812a409fd7503f8bd1eb22f579bd802
patrick wang <patrickwang5000@gmail.com>
2025-03-12 23:54:31 -0500

    Zoo database admin frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css';
-import Message from'./modules/message';
-import TopBar from './modules/topbar';
-import Home from './Pages/Home';
-import SignIn from './Pages/SignIn';
-import {BrowserRouter as Router , Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route, Navigate } from "react-router-dom";
+import { Dashboard, Auth } from "@/layouts";
 
 function App() {
   return (
-    <Router>
-      <div className = "App">
-      <Routes>
-        <Route path = "/" element={<Home />} />
-        <Route path = "/SignIn" element={<SignIn />} />
-      </Routes>
-      </div>
-    </Router>
+    <Routes>
+      <Route path="/dashboard/*" element={<Dashboard />} />
+      <Route path="/auth/*" element={<Auth />} />
+      <Route path="*" element={<Navigate to="/dashboard/home" replace />} />
+    </Routes>
   );
 }
 
