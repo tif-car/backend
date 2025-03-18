56ce602934273d0e044e8b177c2d5c8cabf2464c
Tiffany Carcamo <tif304ca@gmail.com>
2025-03-18 16:36:43 -0500

    Updated fetch URL

--- server.cjs
+++ server.cjs
@@ -18,9 +18,9 @@ const db = mysql.createConnection({
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     port: process.env.DB_PORT,
-    ssl: 
-        { rejectUnauthorized: false }    //change to true later
-  
+    ssl: {
+          ca: fs.readFileSync('/path-to-azure-cert.pem')   //change to true later
+    }
     
 });
 //console.log(process.env.DB_HOST)
