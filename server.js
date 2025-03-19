cdf4b6218338e7e1b65dc42a40395b76d53fbf2e
Tiffany Carcamo <tif304ca@gmail.com>
2025-03-19 11:22:11 -0500

    Updated fetch URL

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ const db = mysql.createConnection({
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     port: process.env.DB_PORT,
-    ssl: { rejectUnauthorized: false }
+    ssl: { rejectUnauthorized: true }
 });
 
 // Connect to MySQL
