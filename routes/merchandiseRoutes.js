b1023765627630f5ef063de3657bda1868bb8b97
R0otB3er <crparke3@cougarnet.uh.edu>
2025-04-20 04:10:35 -0500

    add edit/delete merchandise controllers

--- routes/merchandiseRoutes.js
+++ routes/merchandiseRoutes.js
@@ -2,9 +2,45 @@
 import merchandiseController from "../controllers/merchandiseController.js";
 
 const merchandiseRoutes = {
-  "/api/merchandise": (req, res) => {
+  "/api/merchandise/getDropdowns": (req, res) => {
     if (req.method === "GET") {
-      merchandiseController.getAllMerchandise(req, res);
+      handleRequestBody(req, res, merchandiseController.getDropdowns);
+    } else {
+      res.writeHead(405);
+      res.end("Method Not Allowed");
+    }
+  },
+
+  "/api/merchandise/addMerchandise": (req, res) => {
+    if (req.method === "POST") {
+      handleRequestBody(req, res, merchandiseController.createMerchandise);
+    } else {
+      res.writeHead(405);
+      res.end("Method Not Allowed");
+    }
+  },
+
+  "/api/merchandise/deleteMerchandise": (req, res) => {
+    if (req.method === "POST") {
+      handleRequestBody(req, res, merchandiseController.deleteMerchandiseRow);
+    } else {
+      res.writeHead(405);
+      res.end("Method Not Allowed");
+    }
+  },
+
+  "/api/merchandise/updateMerchandise": (req, res) => {
+    if (req.method === "POST") {
+      handleRequestBody(req, res, merchandiseController.updateMerchandise);
+    } else {
+      res.writeHead(405);
+      res.end("Method Not Allowed");
+    }
+  },
+
+  "/api/merchandise/getMerchByID": (req, res) => {
+    if (req.method === "POST") {
+      handleRequestBody(req, res, merchandiseController.getMerchByID);
     } else {
       res.writeHead(405);
       res.end("Method Not Allowed");
@@ -12,6 +48,21 @@ const merchandiseRoutes = {
   },
 };
 
+// Helper function to parse request body and call the appropriate controller
+function handleRequestBody(req, res, callback) {
+  let body = "";
+  req.on("data", (chunk) => {
+      body += chunk.toString();
+  });
 
+  req.on("end", () => {
+      try {
+          req.body = JSON.parse(body);
+      } catch (error) {
+          req.body = {};
+      }
+      callback(req, res);
+  });
+}
 
 export default merchandiseRoutes;
